6ae809715f1f083046253cbe261726e586ab7384
Thunhaal <thunhaalsuresh2006@gmail.com>
2024-12-21 19:00:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+verb=(input("enter a verb="))
+adjective1=(input("enter a adjective1="))
+adjective2=(input("enter a adjective2="))
+noun=(input("enter a noun="))
+print(f"the zoo is {adjective1}")
+print(f"{adjective1} zoo as an animal {noun}")
+print(f"that {noun} is {adjective2} & {verb}")
